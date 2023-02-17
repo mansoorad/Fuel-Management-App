782aa7617bcf6f1cc4e21ea9e755d58d12b4e600
kumarrishabhjavadeveloper <kumarrishabhjavadeveloper@gmail.com>
2023-02-17 15:50:38 +0530

    dao serive controller exception are added

--- src/main/java/com/ty/fuel_boot/fuelmanagementsystem/dto/Staff.java
+++ src/main/java/com/ty/fuel_boot/fuelmanagementsystem/dto/Staff.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 public class Staff {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private int id;
 	private String name;
 	private String email;
 }
