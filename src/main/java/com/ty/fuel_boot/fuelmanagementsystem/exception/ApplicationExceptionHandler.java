b51dca64e48eb1abc24671e0ce9ee936df77aaaf
abhishekgpjavadeveloper <abhishekgpjavadeveloper@gmail.com>
2023-02-17 17:48:22 +0530

    NoSuchIdFound exception class code is changed

--- src/main/java/com/ty/fuel_boot/fuelmanagementsystem/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/ty/fuel_boot/fuelmanagementsystem/exception/ApplicationExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.ty.fuel_boot.fuelmanagementsystem.exception;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
