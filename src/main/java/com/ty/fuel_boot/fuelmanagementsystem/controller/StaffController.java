b909cc768661636bb69ad2c94e93940899f07b37
GSTharun <gstharun.vs18@gmail.com>
2023-02-18 11:55:15 +0530

    added api detials in supplier controller

--- src/main/java/com/ty/fuel_boot/fuelmanagementsystem/controller/StaffController.java
+++ src/main/java/com/ty/fuel_boot/fuelmanagementsystem/controller/StaffController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -61,7 +62,7 @@ public class StaffController {
 		return staffService.getStaffById(id);
 	}
 
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@PatchMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<ResponseStructure<List<Staff>>> getAllStaff() {
 		return staffService.getAllStaff();
 	}
--- src/main/java/com/ty/fuel_boot/fuelmanagementsystem/controller/SupplierController.java
+++ src/main/java/com/ty/fuel_boot/fuelmanagementsystem/controller/SupplierController.java
@@ -3,6 +3,7 @@ package com.ty.fuel_boot.fuelmanagementsystem.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,29 +18,55 @@ import com.ty.fuel_boot.fuelmanagementsystem.dto.Supplier;
 import com.ty.fuel_boot.fuelmanagementsystem.service.SupplierService;
 import com.ty.fuel_boot.fuelmanagementsystem.util.ResponseStructure;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 public class SupplierController {
 
 	@Autowired
 	private SupplierService supplierService;
 
-	@PostMapping
+	@ApiOperation(value = "Save Supplier", notes = "It is used to save Supplier")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Created"),
+			@ApiResponse(code = 500, message = "Internal server Error"),
+			@ApiResponse(code = 404, message = "Not found"), @ApiResponse(code = 200, message = "ok"),
+			@ApiResponse(code = 403, message = "Forbidden"), @ApiResponse(code = 405, message = "Method Not Allowed") })
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_ATOM_XML_VALUE }, produces = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<ResponseStructure<Supplier>> saveSupplier(@Valid @RequestBody Supplier supplier) {
 		return supplierService.saveSupplier(supplier);
 	}
 
-	@PutMapping
+	@ApiOperation(value = "Update Supplier", notes = "Staff to update Supplier")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 201, message = "Created"),
+			@ApiResponse(code = 500, message = "Internal server error"),
+			@ApiResponse(code = 404, message = "Not found"), @ApiResponse(code = 403, message = "Forbidden"),
+			@ApiResponse(code = 405, message = "Method Not Allowed") })
+	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_ATOM_XML_VALUE })
 	ResponseEntity<ResponseStructure<Supplier>> updateSupplier(@Valid @RequestBody Supplier supplier,
 			@RequestParam int id) {
 		return supplierService.updateSupplier(supplier, id);
 	}
 
-	@GetMapping
+	@ApiOperation(value = "Get Supplier", notes = "Get Supplier by id")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Created"),
+			@ApiResponse(code = 500, message = "Internal server error"),
+			@ApiResponse(code = 404, message = "Not found"), @ApiResponse(code = 200, message = "Ok"),
+			@ApiResponse(code = 403, message = "Forbidden"), @ApiResponse(code = 405, message = "Method Not Allowed") })
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<ResponseStructure<Supplier>> getSupplierById(@Valid @RequestParam int id) {
 		return supplierService.getSupplierById(id);
 	}
 
-	@DeleteMapping("/{id}")
+	@ApiOperation(value = "Delete Supplier", notes = "Use to delete Supplier By given Id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "OK"), @ApiResponse(code = 201, message = "Created"),
+			@ApiResponse(code = 404, message = "Not found"),
+			@ApiResponse(code = 500, message = "Internal server error"),
+			@ApiResponse(code = 403, message = "Forbidden"), @ApiResponse(code = 403, message = "Method Not Allowed") })
+	@DeleteMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	ResponseEntity<ResponseStructure<Supplier>> deleteSupplierById(@Valid @PathVariable int id) {
 		return supplierService.deleteSupplierById(id);
 	}
